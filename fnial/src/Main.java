4f14f9cf2993c77e6ff205c1393d82df35f4022a
AlanSauriel <132103150+AlanSauriel@users.noreply.github.com>
2023-05-25 14:48:51 -0600

    funcionalidades

---
+++ fnial/src/Main.java
@@ -0,0 +1,10 @@
+import static models.principal.totalCigarros;
+
+public class Main {
+    public static void main(String[] args) {
+        presentacion.author070838();
+        totalCigarros();
+
+    }
+
+}
---
+++ fnial/src/presentacion.java
@@ -0,0 +1,8 @@
+public class presentacion {
+    public static void author070838() {
+        String nombre = "Alan S Chan Zepeda";
+        int matricula = 70838;
+        String correo = "al070838@uacam.mx";
+        System.out.println(nombre + "\n" + matricula + "\n" +correo);
+    }
+}
